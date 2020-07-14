da4d696e535733bbf5665b8209cfe47361ae44bc
Saxon Castle <swc006@student.usc.edu.au>
2020-07-15 00:16:16 +1000

    CheatActivity.java - added intent to this class

--- app/src/main/java/android/bignerdranch/com/CheatActivity.java
+++ app/src/main/java/android/bignerdranch/com/CheatActivity.java
@@ -2,9 +2,19 @@ package android.bignerdranch.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "android.bignerdranch.com.geoquiz.answer_is_true";
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
