85dbe2b026796fc5415e7ce0abc3702fb9dd83f4
Saxon Castle <swc006@student.usc.edu.au>
2020-07-15 10:32:46 +1000

    CheatActivity.java - furthered the intent to show if the answer is true or false on button press.

--- app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -125,7 +125,8 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Start cheat activity
-                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
                 startActivity(intent);
             }
         });
